424dc2bb08178ae76fb55a9628a5f971423097d9
ylongtay <145927097+ylongtay@users.noreply.github.com>
2024-05-18 13:29:16 +0800

    Revert "EscapeBall initial template"
    This reverts commit 52c867c6053703992a302acf3e606c9fbf432973.

--- App.js
+++ App.js
@@ -1,63 +1,11 @@
-import React, { useEffect, useState } from 'react';
-import { StyleSheet, View } from 'react-native';
-import { Accelerometer } from 'expo-sensors';
-import { Dimensions } from 'react-native';
-import Svg, { Circle } from 'react-native-svg';
-
-const { width, height } = Dimensions.get('window');
-const ballRadius = 20;
+import { StatusBar } from 'expo-status-bar';
+import { StyleSheet, Text, View } from 'react-native';
 
 export default function App() {
-  const [ballPosition, setBallPosition] = useState({
-    x: width / 2,
-    y: height / 2,
-  });
-
-  const [startTime] = useState(Date.now);
-  const [speedMultiplier, setSpeedMultiplier] = useState(10);
-
-  useEffect(() => {
-    Accelerometer.setUpdateInterval(100);
-
-    const updateSpeedMultiplier = () => {
-      const duration = (Date.now() - startTime) / 1000; // duration in seconds
-      setSpeedMultiplier(100 + duration * 0.1); // Increase multiplier over time
-    };
-
-    const intervalId = setInterval(updateSpeedMultiplier, 1000); // Update every second
-
-    const subscription = Accelerometer.addListener((accelerometerData) => {
-      const { x, y } = accelerometerData;
-      setBallPosition((prevPosition) => {
-        let newX = prevPosition.x + x * speedMultiplier;
-        let newY = prevPosition.y - y * speedMultiplier;
-
-        // Prevent the ball from going off the canvas
-        if (newX + ballRadius > width) newX = width - ballRadius;
-        if (newX - ballRadius < 0) newX = ballRadius;
-        if (newY + ballRadius > height) newY = height - ballRadius;
-        if (newY - ballRadius < 0) newY = ballRadius;
-
-        return { x: newX, y: newY };
-      });
-    });
-
-    return () => {
-      subscription.remove();
-      clearInterval(intervalId);
-    };
-  }, [speedMultiplier]);
-
   return (
     <View style={styles.container}>
-      <Svg height={height} width={width}>
-        <Circle
-          cx={ballPosition.x}
-          cy={ballPosition.y}
-          r={ballRadius}
-          fill="blue"
-        />
-      </Svg>
+      <Text>Open up App.js to start working on your app!</Text>
+      <StatusBar style="auto" />
     </View>
   );
 }
@@ -65,5 +13,8 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
   },
 });
